b29f2c0ab715bfd657cec7bdb134e0a245fd9143
ymittalunisa <yashmit178@gmail.com>
2025-08-03 22:33:10 +0200

    fb db1 connected

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,21 @@ import 'package:ble/src/controllers/service/local_services.dart';
 import 'package:ble/src/controllers/service/service_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "dummy",
+      // Not needed for Realtime DB-only usage
+      appId: "dummy",
+      messagingSenderId: "dummy",
+      projectId: "dummy",
+      databaseURL:
+          "https://classroom-6206e-default-rtdb.europe-west1.firebasedatabase.app/",
+    ),
+  );
   runApp(
     MultiRepositoryProvider(
       providers: [
@@ -21,8 +34,7 @@ void main() {
         providers: [
           BlocProvider<AuthBloc>(
               create: (context) =>
-                  AuthBloc(AuthRepository(context.read<ServiceRepository>()))
-                    ..add(AppStarted())),
+                  AuthBloc(AuthRepository())..add(AppStarted())),
           BlocProvider<BleBloc>(
             create: (context) =>
                 BleBloc(BleRepository(context.read<ServiceRepository>())),
--- lib/src/controllers/auth/auth_repository.dart
+++ lib/src/controllers/auth/auth_repository.dart
@@ -1,36 +1,26 @@
-import 'package:ble/src/controllers/service/service_repository.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class AuthRepository {
-  final storage = const FlutterSecureStorage();
-  final ServiceRepository _serviceRepository;
+  final _dbRef = FirebaseDatabase.instance.ref().child('users');
 
-  AuthRepository(this._serviceRepository);
+  Future<void> deleteToken() async {}
 
-  Future<void> deleteToken() async => await storage.delete(key: 'jwt_token');
+  Future<void> persistToken(String token) async {}
 
-  Future<void> persistToken(String token) async =>
-      await storage.write(key: 'jwt_token', value: token);
+  Future<String?> getToken() async => null;
 
-  Future<String?> getToken() async => await storage.read(key: 'jwt_token');
-
-  Future<bool> hasToken() async {
-    String? value = await getToken();
-    // TODO: check if token is still valid
-    return (value != null) ? true : false;
-  }
+  Future<bool> hasToken() async => false;
 
   Future<bool> login(String username, String password) async {
-    final response = await _serviceRepository.login(username, password);
-    if (response.status) {
-      persistToken(response.data);
-      return true;
+    final DataSnapshot snapshot = await _dbRef.child(username).get();
+    if (snapshot.exists) {
+      final data =
+          Map<String, dynamic>.from(snapshot.value as Map<dynamic, dynamic>);
+      return data['password'] == password;
     } else {
       return false;
     }
   }
 
-  Future<void> logout() async {
-    await deleteToken();
-  }
+  Future<void> logout() async {}
 }
