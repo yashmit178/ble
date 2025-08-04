52f9f1b99ffe966e88490b7d5feaa3d3cd0dbd1b
ymittalunisa <yashmit178@gmail.com>
2025-08-04 22:17:21 +0200

    added shared_preferences to remember user

--- lib/src/controllers/auth/auth_repository.dart
+++ lib/src/controllers/auth/auth_repository.dart
@@ -1,26 +1,42 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthRepository {
   final _dbRef = FirebaseDatabase.instance.ref().child('users');
+  static const _authKey = 'is_authenticated';
 
-  Future<void> deleteToken() async {}
+  Future<void> deleteToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_authKey);
+  }
 
-  Future<void> persistToken(String token) async {}
+  Future<void> persistToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_authKey, true);
+  }
 
-  Future<String?> getToken() async => null;
-
-  Future<bool> hasToken() async => false;
+  Future<bool> hasToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_authKey) ?? false;
+  }
 
   Future<bool> login(String username, String password) async {
     final DataSnapshot snapshot = await _dbRef.child(username).get();
     if (snapshot.exists) {
       final data =
           Map<String, dynamic>.from(snapshot.value as Map<dynamic, dynamic>);
-      return data['password'] == password;
+      if (data['password'] == password) {
+        await persistToken();
+        return true;
+      } else {
+        return false;
+      }
     } else {
       return false;
     }
   }
 
-  Future<void> logout() async {}
+  Future<void> logout() async {
+    await deleteToken();
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_database
 import flutter_blue_plus_darwin
 import flutter_secure_storage_darwin
 import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
